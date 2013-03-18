5b613944b571a829a09a1124670d4632f1c227b4
Matthias Rothe <mimarox@users.sourceforge.net>
2013-03-18 07:49:18 +0000

    Added multi threading support

--- testng-databinding-core/src/main/java/net/sf/testng/databinding/GenericDataProvider.java
+++ testng-databinding-core/src/main/java/net/sf/testng/databinding/GenericDataProvider.java
@@ -94,7 +94,7 @@ public class GenericDataProvider {
 	private GenericDataProvider() {
 	}
 
-	private static DataProviderStrategiesMap getStrategies() {
+	private synchronized static DataProviderStrategiesMap getStrategies() {
 		if (strategies == null) {
 			strategies = new DataProviderStrategiesMap();
 
