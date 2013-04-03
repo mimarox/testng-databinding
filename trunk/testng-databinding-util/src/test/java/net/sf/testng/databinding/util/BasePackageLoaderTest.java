969505275a7235e11efd0e3e17e126cd88da73c5
mimarox <mimarox@f0c048fc-4950-4f78-b8b0-c4816e2d105c>
2013-04-03 20:41:21 +0000

    Fixed unit test and implemented one new unit test

--- trunk/testng-databinding-util/src/test/java/net/sf/testng/databinding/util/BasePackageLoaderTest.java
+++ trunk/testng-databinding-util/src/test/java/net/sf/testng/databinding/util/BasePackageLoaderTest.java
@@ -1,6 +1,7 @@
 package net.sf.testng.databinding.util;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertSame;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
@@ -10,6 +11,7 @@ import java.util.Set;
 import org.testng.annotations.Test;
 
 public class BasePackageLoaderTest {
+	private Set<String> cached;
 
 	@Test
 	public void shouldLoadBasePackagesFromReader() throws Exception {
@@ -32,7 +34,15 @@ public class BasePackageLoaderTest {
 	@Test(dependsOnMethods = "shouldLoadBasePackagesFromReader")
 	public void shouldLoadBasePackagesFromClasspath() {
 		Set<String> basePackages = BasePackageLoader.loadBasePackages("testng-databinding.base-packages");
-		String[] expecteds = new String[] { "com", "net.sf", "org.testng" };
+		String[] expecteds = new String[] { "com", "net.sf", "org.testng", "net.sf.testng" };
 		assertEquals(basePackages.toArray(), expecteds);
+
+		cached = basePackages;
+	}
+
+	@Test(dependsOnMethods = "shouldLoadBasePackagesFromClasspath")
+	public void shouldLoadBasePackagesFromCache() {
+		Set<String> basePackages = BasePackageLoader.loadBasePackages("testng-databinding.base-packages");
+		assertSame(basePackages, cached);
 	}
 }
