00dee60a4e7004200460dc67aa92ac0dc3cbb249
mimarox <mimarox@f0c048fc-4950-4f78-b8b0-c4816e2d105c>
2013-04-13 16:10:29 +0000

    Updated JavaDoc

--- trunk/testng-databinding-csv/src/main/java/net/sf/testng/databinding/csv/CsvFileAtOnceDataSource.java
+++ trunk/testng-databinding-csv/src/main/java/net/sf/testng/databinding/csv/CsvFileAtOnceDataSource.java
@@ -1,6 +1,7 @@
 package net.sf.testng.databinding.csv;
 
 import java.lang.reflect.Type;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,29 +10,33 @@ import java.util.Properties;
 
 import net.sf.testng.databinding.AbstractDataSource;
 import net.sf.testng.databinding.DataSource;
+import net.sf.testng.databinding.IDataSource;
 import net.sf.testng.databinding.TestInput;
 import net.sf.testng.databinding.TestOutput;
 import net.sf.testng.databinding.core.error.ErrorCollector;
 import net.sf.testng.databinding.core.error.MissingPropertiesException;
 import net.sf.testng.databinding.core.error.MultipleConfigurationErrorsException;
+import net.sf.testng.databinding.core.error.MultipleSourceErrorsException;
 import net.sf.testng.databinding.core.util.Types;
 import net.sf.testng.databinding.util.MethodParameter;
 
 /**
- * Reads a whole CSV file into the first and only object contained in the first and only Object
- * array returned by calling {@link #next()}. This object needs to be a {@link List} of either
- * primitives as defined by {@link Types#isPrimitiveType(Type)} or objects of some Java Bean type.
  * <p>
+ * This {@link IDataSource data source} binds test data contained in CSV files to a test method parameter. It supports
+ * only one {@link TestInput test input} parameter which needs to be a {@link List} of either primitives as defined
+ * by {@link Types#isPrimitiveType(Type)} or objects of some Java Bean type. A {@link List} of primitives or Java
+ * Beans is supported as a test output parameter with the appropriate <code>mapper</code>. More on that below.
+ * </p><p>
  * Supposedly using this data source only makes sense if the test method getting its data this way has
  * just one fixed way of input and a predefined set of expected results. Then the list will contain
  * exactly these results as read from the CSV file.
- * <p>
- * The default mapper for this data source is the {@link HeaderNameMapper}. It
- * may be overridden by the mapper property.
+ * </p>
+ * <h3>Specifications</h3>
+ * <h4>Data Properties</h4>
  * <p>
  * The following table gives an overview of the required and optional data property keys for this
  * data source.
- * <p>
+ * </p><p>
  * <table border="1">
  * <tr>
  * <td><b>Key</b></td>
@@ -42,15 +47,16 @@ import net.sf.testng.databinding.util.MethodParameter;
  * </tr>
  * <tr>
  * <td>dataSource</td>
- * <td><code>csv-file-at-once</code></td>
+ * <td><code>csv</code></td>
  * <td>N/A</td>
  * <td>The name of this data source</td>
  * <td>Yes</td>
  * </tr>
  * <tr>
  * <td>url</td>
- * <td>An absolute or relative source file locator<br>
- * A relative locator must start with a slash (/)</td>
+ * <td>A {@link URL} conformant {@link String} for an absolute<br>
+ * locator or a relative path starting with a<br>
+ * slash (/)</td>
  * <td>N/A</td>
  * <td>The locator of the actual data source file</td>
  * <td>Yes</td>
@@ -63,7 +69,7 @@ import net.sf.testng.databinding.util.MethodParameter;
  * mappers:<br><br>
  * <code>headerNameMapper</code><br>
  * <code>headerNameFileLinkingMapper</code></td>
- * <td><code>headerName</code></td>
+ * <td><code>headerNameMapper</code></td>
  * <td>The mapper implementation defining how the data within the<br>
  * CSV source file will be mapped to the test method parameters</td>
  * <td>No</td>
@@ -149,6 +155,25 @@ import net.sf.testng.databinding.util.MethodParameter;
  * <td>No</td>
  * </tr>
  * </table>
+ * </p>
+ * <h4>CSV Data Files</h4>
+ * <p>
+ * The structure of the CSV files depends a lot on the specified data properties and the selected <code>mapper</code>.
+ * See the JavaDocs for the standard mappers {@link HeaderNameMapper} and {@link HeaderNameFileLinkingMapper}.
+ * </p>
+ * <h4>Java Beans</h4>
+ * <p>
+ * Java Beans that data is to be bound to need to have a standard constructor taking no arguments. Only primitive types and
+ * {@link String Strings} are supported as Java Bean property types. Nested Java Beans are not supported either. If there
+ * are any Java Bean properties for which no columns can be found a {@link MultipleSourceErrorsException} is thrown. Any
+ * Java Bean properties having an unsupported type are just not set, leaving them unchanged. Any columns for which no Java
+ * Bean properties of matching name and supported type can be found are skipped ignoring them.
+ * </p>
+ * <h3>Examples</h3>
+ * <p>
+ * All examples for binding CSV files are given within the standard mapper classes, as any CSV file binding is actually
+ * mapper dependent.
+ * </p>
  * 
  * @author Matthias Rothe
  */
@@ -223,11 +248,17 @@ public class CsvFileAtOnceDataSource extends AbstractDataSource {
 		return properties;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean hasNext() {
 		return this.delegate.hasNext();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Object[] next() {
 		final List<Object> list = new ArrayList<Object>();
